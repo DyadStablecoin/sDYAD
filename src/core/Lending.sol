4f669121c3cbf7df372491fc093c80420339ca97
shafu.eth <selfouly@gmail.com>
2024-07-30 12:48:02 +0300

    calc interest

--- src/core/Lending.sol
+++ src/core/Lending.sol
@@ -1,38 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {IDyad}  from "../interfaces/IDyad.sol";
-import {IVault} from "../interfaces/IVault.sol";
+import {sDYAD}         from "./sDYAD.sol";
+import {IDyad}         from "../interfaces/IDyad.sol";
+import {IVault}        from "../interfaces/IVault.sol";
+import {IAggregatorV3} from "../interfaces/IAggregatorV3.sol";
 
 import {FixedPointMathLib} from "@solmate-6.7.0/src/utils/FixedPointMathLib.sol";
+import {SafeCast}          from "@openzeppelin-contracts-5.0.2/utils/math/SafeCast.sol";
+
 
 contract Lending {
   using FixedPointMathLib for uint;
+  using SafeCast          for int;
 
-  uint public constant k = 0.1e18;
+  error StaleData();
 
-  IDyad  public dyad;
-  IVault public ethVault;
+  uint          public constant  K = 0.1e18;
+  uint          public constant  STALE_DATA_TIMEOUT = 90 minutes; 
+  IAggregatorV3 public immutable oracle;
 
-  uint  public totalDyadLent;
+  IDyad public dyad;
+  sDYAD public sDyad;
   uint  public totalDyadBorrowed;
+  uint  public dyadInVault;
+  uint  public totalCollatValue;
 
-  constructor(
-    IDyad  _dyad,
-    IVault _ethVault
-  ) {
-    dyad     = _dyad;
-    ethVault = _ethVault;
+  struct Loan {
+    uint collat;
+    uint debt;
+    uint interest;
+    uint lastPaymentTime;
   }
 
-  function getInterestRate(uint id) 
+  mapping(address => Loan) public loans;
+
+  constructor(
+    IDyad         _dyad,
+    sDYAD         _sDyad,
+    IAggregatorV3 _oracle
+  ) {
+    dyad   = _dyad;
+    sDyad  = _sDyad;
+    oracle = _oracle;
+  }
+
+  function interest(uint dyadAmount) 
     public 
     view 
     returns (uint) 
   {
-    uint eth = ethVault.id2asset(id).mulWadDown(ethVault.assetPrice());
-    uint a   = eth.mulWadDown(dyad.balanceOf(address(ethVault)));
-    uint b   = (totalDyadBorrowed**2).mulWadDown(a);
-    return k.mulWadDown(b);
+    uint newTotalDyadBorrowed = totalDyadBorrowed + dyadAmount;
+
+    // totalDyadDeployed^2
+    uint totalDyadBorrowdSquared = newTotalDyadBorrowed.mulWadDown(newTotalDyadBorrowed);
+
+    // (totalDyadDeployed^2) / dyadInVault
+    uint ratio = totalDyadBorrowdSquared.divWadDown(dyadInVault);
+
+    // k * ratio
+    uint result = K.mulWadDown(ratio);
+
+    // final result: (k * (totalDyadDeployed^2)) / (dyadInVault * totalCollatValue)
+    return result.divWadDown(totalCollatValue);
+  }
+
+    function ethPrice() 
+    public 
+    view 
+    returns (uint) {
+      (
+        ,
+        int256 answer,
+        , 
+        uint256 updatedAt, 
+      ) = oracle.latestRoundData();
+      if (block.timestamp > updatedAt + STALE_DATA_TIMEOUT) revert StaleData();
+      return answer.toUint256();
   }
 }
--- test/fork/Lending.t.sol
+++ test/fork/Lending.t.sol
@@ -5,22 +5,25 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import {Parameters} from "../../src/Parameters.sol";
 import {Lending} from "../../src/core/Lending.sol";
+import {sDYAD} from "../../src/core/sDYAD.sol";
 import {IDyad} from "../../src/interfaces/IDyad.sol";
 import {IVault} from "../../src/interfaces/IVault.sol";
 
 contract LendingTest is Test, Parameters {
   Lending public lending;
 
-  function setUp() public {
-    lending = new Lending(
-      IDyad(MAINNET_V2_DYAD),
-      IVault(MAINNET_V2_WETH_VAULT)
-    );
-  }
+  // function setUp() public {
+  //   sDYAD sDyad = new sDYAD();
 
-  function testA() public {
-    uint interestRate = lending.getInterestRate(0);
-    console.log("Interest Rate: %d", interestRate);
+  //   lending = new Lending(
+  //     IDyad(MAINNET_V2_DYAD),
+  //     sDyad,
+  //     IVault(MAINNET_V2_WETH_VAULT)
+  //   );
+  // }
 
-  }
+  // function testA() public {
+  //   uint interest = lending.interest(0);
+  //   console.log("Interest Rate: %d", interest);
+  // }
 }
