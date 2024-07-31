3b0c3a5921703e9bb68eb155aef379de467c14ff
Coffee☕️ <coffee@coffeebot.dev>
2024-07-31 09:47:35 -0400

    add ability to borrow from loan manager; virtual accounting for borrowed assets.

--- src/core/sDYAD.sol
+++ src/core/sDYAD.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.25;
 import {Ownable} from "@solady/auth/Ownable.sol";
 import {ERC4626} from "@solady/tokens/ERC4626.sol";
 import {UUPSUpgradeable} from "@solady/utils/UUPSUpgradeable.sol";
+import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
 
 contract sDYAD is ERC4626, UUPSUpgradeable, Ownable {
     address constant DYAD = 0xFd03723a9A3AbE0562451496a9a394D2C4bad4ab;
-
+    
     uint8 version;
+    address loanManager;
 
+    uint256 public totalBorrowed;
+    
     constructor() {
         version = type(uint8).max;
     }
@@ -33,8 +37,31 @@ contract sDYAD is ERC4626, UUPSUpgradeable, Ownable {
         return "sDYAD";
     }
 
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+    /// @notice Borrows assets from the vault - only callable by the loan manager
+    /// @param amount The amount to borrow
+    /// @param to The address to send the borrowed assets to
+    function borrow(uint256 amount, address to) external {
+        if (msg.sender != loanManager) {
+            revert Unauthorized();
+        }
+        totalBorrowed += amount;
+        SafeTransferLib.safeTransfer(DYAD, to, amount);
+    }
+
+    /// @notice Reduce the total borrowed amount - only callable by the loan manager
+    /// to be called in the event of a liquidation that results in a loss, or a repayment
+    /// @param amount The amount to reduce the total borrowed amount by
+    function reduceBorrowed(uint256 amount) external {
+        if (msg.sender != loanManager) {
+            revert Unauthorized();
+        }
+        totalBorrowed -= amount;
+    }
+
+    /// @notice Total amount of assets managed by the vault, including
+    /// the total amount currently lent out via bonds.
+    function totalAssets() public view override returns (uint256) {
+        return super.totalAssets() + totalBorrowed;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
